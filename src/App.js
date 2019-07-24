8b855386885d0d9b0185e39c502bc07c8ee94261
Michael Barachkov <mike.barachkov@outlook.com>
2019-07-23 22:53:13 -0700

    added the functionality of a basic version of the app using the extended session example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChihuahuaImage from './components/GameImage'
+import images from './images.json'
+
+import './App.css'
+
+// pick a random number up to but not including max
+const pickRandom = max => Math.floor(Math.random() * max)
+
+// pick out a random pup
+const randomImageId = () => images[pickRandom(images.length)].id
+
+// track
+class App extends React.Component {
+  // define initial state values
+  state = {
+    clicks: 0,
+    score: 0,
+    currentPupId: randomImageId()
+  }
+
+  // when image is clicked
+  handleImageClick = id => {
+    // update click count in state
+    const newState = { clicks: this.state.clicks + 1 }
+
+    // did the user click the current pup
+    if (id === this.state.currentPupId) {
+      // increase score
+      newState.score = this.state.score + 1
+      newState.currentPupId = randomImageId()
+    }
+
+    this.setState(newState)
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Chihuahua Clicker</h1>
+        <div>Score: {this.state.score}</div>
+        <div>Clicks: {this.state.clicks}</div>
+        {/* reference images in the public folder */}
+        {images.map(image => (
+          <ChihuahuaImage
+            key={image.id}
+            handleImageClick={() => this.handleImageClick(image.id)}
+            url={process.env.PUBLIC_URL + '/img/' + image.fileName}
+          />
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
